e6d63ff704201a5714a5e366da95bb90c2e43ef3
mansid-hub <mansid1100@gmail.com>
2025-10-13 11:21:01 +0530

    Update Branches.tsx

--- src/pages/Branches.tsx
+++ src/pages/Branches.tsx
@@ -30,7 +30,7 @@ const Branches = () => {
     },
     {
       name: "Manmad Branch", 
-      address: "Shop No. 456, Main Road, Manmad, Near Bus Stand, Maharashtra 423104",
+      address: "First floor Hemsuraj kirana, Near Sundar Selection Aajad Road, Manmad, Maharashtra 423104",
       phone: "+91-8208368355",
       timings: "Monday - Friday: 8:45 AM - 2:00 PM | Saturday - Sunday: 10:00 AM - 3:00 PM",
       studentsCount: "150+",
